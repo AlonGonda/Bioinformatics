0959cc6fa1022cb97f6577f532c5b7e043623a61
אלון גונדא <alongond@post.bgu.ac.il>
2022-04-07 15:57:48 +0300

    change folder directory in main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from functions import a_b_distance, calculateDistanceMatrix, read_ca_coordinates, a_b_distance_with_grad
 
 
@@ -6,11 +8,10 @@ def main():
     print(a_b_distance(10, 5))
     # print("Example of a_b_dm")
     # print(a_b_dm(10, 5))
-
     distanceMatrix = calculateDistanceMatrix(
-        "C:/Users/user/Desktop/third year/Bioinformatics/refinementSampleData")  # Calculated Matrix Distances for a certain protein
+        os.getcwd() + "/refinementSampleData")  # Calculated Matrix Distances for a certain protein
     print("Example of loading protein Coordinates")
-    read_ca_coordinates("C:/Users/user/Desktop/third year/Bioinformatics/refinementSampleData")
+    read_ca_coordinates(os.getcwd() + "/refinementSampleData")
     print("Back to a_b_distance this time with gradient")
     print(a_b_distance_with_grad(10., 5.))
 
